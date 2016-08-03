62ec05977f1bc19f613392fd27a4c60d2a9410f5
PakostiN <gravediggaz@abv.bg>
2016-08-03 15:00:40 +0300

    homework8

---
+++ Homework8/Task 9/scripts.js
@@ -0,0 +1,11 @@
+var person = {
+	name: "Ivan Ivanov",
+	years: "25",
+	exp: "Medium",
+	studing: function() {
+		console.log("person is studing");
+	}
+	fun: function() {
+		console.log("person is having fun");
+	}	
+}
